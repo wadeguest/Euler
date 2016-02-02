fd76396ca0bf4ad0f224d29395d581d0befc997a
wadeguest <wade.guest@hotmail.ca>
2016-02-01 23:59:23 -0600

    Euler Problem 1 and designed basic interface

---
+++ Euler/Euler/EulerImplementation.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Euler
+{
+    partial class frEulerImplementation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(0, 13);
+            this.lblDescription.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 152);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 2;
+            // 
+            // frEulerImplementation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 455);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDescription);
+            this.Name = "frEulerImplementation";
+            this.Text = "EulerImplementation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ Euler/Euler/MainSelect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Euler
+{
+    public partial class frMain : Form
+    {
+        public frMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbEulerProblem.Items.Add("1 - Multiples of 3 and 5");
+            cmbEulerProblem.SelectedIndex = 0;
+
+        }
+
+        private void btnEuler_Click(object sender, EventArgs e)
+        {
+            String cmbString = cmbEulerProblem.Text.Substring(0, cmbEulerProblem.Text.Split('-').Length-1);
+            int eulerNum = Int32.Parse(cmbString);
+            Form EulerImpForm = new frEulerImplementation(eulerNum);
+            EulerImpForm.Show();
+
+        }
+    }
+}
